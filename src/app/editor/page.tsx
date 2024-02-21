222d701501f0d95d33fb6307b05501f105a95622
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2024-02-21 22:39:34 +0530

    impliment socket io but not working git add .

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,8 +1,26 @@
 "use client";
 import { Editor } from "@monaco-editor/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
+const socket = io("http;//localhost:3001");
 
 const EditorPage = () => {
+  const [code, setCode] = useState<string[]>([]);
+  const [newCode, setNewCode] = useState(1);
+
+  // useEffect(() => {
+  //   socket.on("code", (code) => {
+  //     setCode((prevCode) => [...prevCode, code]);
+  //   });
+  // }, []);
+  //
+  // const sendCode = () => {
+  //   socket.emit("code", newCode);
+  //   setNewCode("");
+  // };
+  // console.log(code);
+  function change(value) {}
   return (
     <div>
       <div>EditorPage</div>
@@ -11,8 +29,11 @@ const EditorPage = () => {
           height="90vh"
           width="90vw"
           defaultLanguage="javascript"
-          defaultValue="//work here"
+          defaultValue="//commet"
           theme="vs-dark"
+          onChange={(value) => {
+            console.log(value);
+          }}
         />
       </div>
     </div>
