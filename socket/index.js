658e6dcecb7966f68bde4d96cc4fd8652b07d9f8
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2024-02-22 18:03:11 +0530

    created socket backend

---
+++ socket/index.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var http = require("http");
+var socketIo = require("socket.io");
+var cors = require("cors");
+var app = express();
+var server = http.createServer(app);
+var io = socketIo(server, {
+    cors: {
+        origin: "*",
+    },
+});
+app.use(cors());
+io.on("connection", function (socket) {
+    console.log("A user connected");
+    socket.on("code", function (newCode) {
+        io.emit("code", newCode);
+    });
+    // socket.on("disconnect", () => {
+    //   console.log("A user disconnected");
+    // });
+});
+var PORT = 3001;
+server.listen(PORT, function () {
+    console.log("listening on port ".concat(PORT));
+});
