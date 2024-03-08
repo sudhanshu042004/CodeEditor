ca758a805d9763188b0e3ea2f323021c017321c3
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2024-03-08 15:02:52 +0530

    code refactor

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
 import { z } from "zod"
 import { compareSync } from "bcrypt"
@@ -6,31 +7,44 @@ import { compareSync } from "bcrypt"
 
 const prisma = new PrismaClient();
 const UserSchema = z.object({
-    email: z.string().email(),
-    name: z.string().optional(),
-    password: z.string().min(8),
+  email: z.string().email(),
+  name: z.string().optional(),
+  password: z.string().min(8),
 })
+
+type User = z.infer<typeof UserSchema>
 
 //authenticate user
 export async function POST(req: NextRequest) {
-    const body = await req.json();
+  const body = await req.json();
 
-    //zod
-    const { success } = UserSchema.safeParse(body);
-    if (!success) {
-        return NextResponse.json({ error: "invalid input" }, { status: 400 })
+  //zod
+  const { success, data } = UserSchema.safeParse(body) as { success: boolean; data: User };
+  if (!success) {
+    return NextResponse.json({ error: "invalid input" }, { status: 400 })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: data.email
     }
+  })
 
-    const user = await prisma.user.findUnique({
-        where: {
-            email: body.email
-        }
-    })
+  if (!user) {
+    return NextResponse.json({ error: "there is no account found" }, { status: 404 })
+  }
+  const password = compareSync(data.password, user.password)
 
-    const password = compareSync(body.password, user.password)
-    if (!password) {
-        return NextResponse.json({ error: "invalid password" }, { status: 401 })
-    }
+  if (!password) {
+    return NextResponse.json({ error: "invalid password" }, { status: 401 })
+  }
 
-    return NextResponse.json({ response: "account login" });
+  // JWT token 
+  const JWT_SECRET = process.env.JWT_SECRET;
+  if (!JWT_SECRET) {
+    return NextResponse.json({ error: "JWT_SECRET is undefined" })
+  }
+  const jwtToken = jwt.sign({ userId: user.id }, JWT_SECRET);
+
+  return NextResponse.json({ response: "account login", token: jwtToken });
 }
