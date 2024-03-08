3c5d37f42f904ec551e1ca2783a9dbe367abb06a
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2024-03-08 15:19:13 +0530

    implement jwt

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -2,34 +2,42 @@ import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
 
 const UserSchema = z.object({
-    email : z.string().email(),
-    name  : z.string().optional(),
-    password : z.string().min(8),
+  email: z.string().email(),
+  name: z.string().optional(),
+  password: z.string().min(8),
 })
-
+type User = z.infer<typeof UserSchema>
 const prisma = new PrismaClient();
 
 //signup
-export async function POST (req : NextRequest) {
-    const body = await req.json();
+export async function POST(req: NextRequest) {
+  const body = await req.json();
 
-    //zod validation
-    const { success } = UserSchema.safeParse(body);
-    if (!success){
-        return NextResponse.json({ error: 'Invalid input' }, { status: 400 })
+  //zod validation
+  const { success, data } = UserSchema.safeParse(body) as { success: boolean, data: User };
+  if (!success) {
+    return NextResponse.json({ error: 'Invalid input' }, { status: 400 })
+  }
+
+  //password hashing
+  const password = bcrypt.hashSync(data.password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      name: data.name,
+      email: data.email,
+      password: password
     }
+  })
+  //jwt Token
+  const JWT_SECRET = process.env.JWT_SECRET;
+  if (!JWT_SECRET) {
+    return NextResponse.json({ error: "jWT_SECRET is undefined" });
+  }
+  const jwtToken = jwt.sign({ userId: user.id }, JWT_SECRET);
 
-    //password hashing
-    const password = bcrypt.hashSync(body.password,10);
-
-    const user = await prisma.user.create({
-        data : {
-            name : body.name,
-            email : body.email,
-            password : password
-        }
-    })
-    return NextResponse.json({response : "account successfully created"});
+  return NextResponse.json({ response: "account successfully created", token: jwtToken });
 }
