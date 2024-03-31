90402dcf0ad00081ac4a258e1fe786a206570e45
Sudhanshu <sudhanshusaudiyal15@gmail.com>
2024-03-31 22:12:56 +0530

    add redirect in dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,24 @@
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation';
 import React from 'react'
+import jwt from "jsonwebtoken";
 
-const dashboard= () => {
+const dashboard = () => {
+  const cookiesStore = cookies();
+  const cookie = cookiesStore.get('Authorization');
+  if (!cookie) {
+    return redirect("/login");
+  }
+  const token = cookie.value.split(" ")[1];
+  const jwtSecret = process.env.JWT_SECRET;
+  console.log(jwtSecret);
+  if (jwtSecret === undefined) {
+    return console.log("jwt secret is undefined")
+  }
+  const tokenVerified = jwt.verify(token, jwtSecret);
+  if (!tokenVerified) {
+    return redirect("/login");
+  }
   return (
     <div>dashboard</div>
   )
